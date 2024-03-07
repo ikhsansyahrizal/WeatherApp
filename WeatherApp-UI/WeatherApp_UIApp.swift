bbf1e958c937098d2e5eb64c2371bad19f896d52
ikhsansyahrizal <ikhsansyahrizal23@gmail.com>
2024-03-07 08:43:24 +0700

    Initial Commit

---
+++ WeatherApp-UI/WeatherApp_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp_UIApp.swift
+//  WeatherApp-UI
+//
+//  Created by Ikhsan on 07/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
